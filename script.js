539ad632813c88c2df11f2b4083ff1fb8788ec0d
Giga <gigashekelashvili1@gmail.com>
2025-12-03 16:50:37 +0400

    Lecture-23-Homework

---
+++ script.js
@@ -0,0 +1,49 @@
+// number 1
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum = numbers.reduce((acc, curr) => {
+//   return (acc = acc + curr);
+// }, 0);
+
+// console.log(sum);
+
+// number 2
+
+// let numbers = [20, 13, 30, 78, 102, 32, 1];
+
+// sum = numbers.reduce((acc, curr) => {
+//   if (acc > curr) {
+//     return acc;
+//   } else {
+//     return curr;
+//   }
+// }, numbers[0]);
+
+// console.log(sum);
+
+// number 4
+
+// let words = ["world", "is", "yours"];
+
+// console.log(words.join("-"));
+
+// number 5
+
+// let numbers = [10, 20, 30];
+
+// sum = numbers.reduce((acc, curr) => {
+//   return acc * curr;
+// }, 1);
+
+// console.log(sum);
+
+//number 6
+
+// let numbers = [30, 70, 110];
+
+// diff = numbers.reduce((acc, curr) => {
+//   return acc - curr;
+// });
+
+// console.log(diff);
